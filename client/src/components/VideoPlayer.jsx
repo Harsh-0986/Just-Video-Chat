3de66e1637ec2bcf5b1fdc8d7dd7046f41e20a82
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-10 12:45:40 +0530

    Creating Notifications Component

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -7,6 +7,7 @@ import { SocketContext } from "../SocketContext";
 const useStyles = makeStyles((theme) => ({
   video: {
     width: "550px",
+    height: "50vh",
     [theme.breakpoints.down("xs")]: {
       width: "300px",
     },
