1b25c73bc5c5d1fa200e9e1f1aa7a04a480d3b2d
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-10 12:17:18 +0530

    Creating Options Component

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Grid, Typography, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { SocketContext } from "../SocketContext";
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: "550px",
+    [theme.breakpoints.down("xs")]: {
+      width: "300px",
+    },
+  },
+  gridContainer: {
+    justifyContent: "center",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column",
+    },
+  },
+  paper: {
+    padding: "10px",
+    border: "2px solid black",
+    margin: "10px",
+  },
+}));
 
 const VideoPlayer = () => {
-    return (
-        <div>
-            VideoPlayer            
-        </div>
-    )
-}
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
+  const classes = useStyles();
 
-export default VideoPlayer
+  return (
+    <Grid container className={classes.gridContainer}>
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom>
+              {name || "Name"}
+            </Typography>
+            {/* Our Video */}
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom>
+              {call.name || "Name"}
+            </Typography>
+            {/* User's Video */}
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
+};
+
+export default VideoPlayer;
