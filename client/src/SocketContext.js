20c253244d0a45bffeeb3b5007213d657255cc7d
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-06-10 13:11:29 +0530

    Deploying to github pages

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+const socket = io("https://just-video-chat-server.herokuapp.com/");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
